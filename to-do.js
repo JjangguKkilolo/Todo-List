fe5b3ff3ab4e4e43b2b1b2a4fd7b106a785c703f
JjangguKkilolo <seungjin001115@gmail.com>
2026-07-16 12:54:10 +0900

    fear: add todo list starter

---
+++ to-do.js
@@ -0,0 +1,57 @@
+const todoForm = document.querySelector("#todoForm");
+const todoInput = document.querySelector("#todoInput");
+const todoList = document.querySelector("#todoList");
+
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const text = todoInput.value.trim();
+
+  if (text === "") {
+    alert("할 일을 입력해 주세요!");
+    todoInput.focus();
+    return;
+  }
+
+  addTodo(text);
+
+  todoInput.value = "";
+
+  todoInput.focus();
+});
+
+function addTodo(todoText) {
+    const todoItem = document.createElement("li");
+    todoItem.className = "todo-item";
+
+    const todoTextSpan = document.createElement("span");
+    todoTextSpan.textContent = todoText;
+    
+    const actionBox = document.createElement("div");
+    actionBox.className = "todo-actions";
+
+    const doneButton = document.createElement("button");
+    doneButton.type = "button";
+    doneButton.className = "done-button";
+    doneButton.textContent = "완료";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.type = "button";
+    deleteButton.className = "delete-button";
+    deleteButton.textContent = "삭제";
+
+    doneButton.addEventListener("click", function () {
+        todoItem.classList.toggle("done");
+    });
+    
+    deleteButton.addEventListener("click", function () {
+        todoItem.remove();
+    });
+
+    actionBox.appendChild(doneButton);
+    actionBox.appendChild(deleteButton);
+
+    todoItem.appendChild(todoTextSpan);
+    todoItem.appendChild(actionBox);
+
+    todoList.appendChild(todoItem);
+}
